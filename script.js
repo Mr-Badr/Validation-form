29d1bd596283ce63ec046ca2473a6d93f0d690b3
Badr Eddine El Harchali <Mr.Elharchali@gmail.com>
2021-08-13 21:34:08 +0100

    Add HTML & CSS

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("form");
+const name = document.getElementById("name");
+const password = document.getElementById("password");
+const error = document.getElementById("error");
+
+addEventListener("submit", (e) => {
+  e.preventDefault();
+});
